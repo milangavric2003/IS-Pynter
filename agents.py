906cdd23a8b09fb913f7c410674136d03e7e62c9
Milan <milangavric2003@gmail.com>
2026-01-28 23:41:51 +0100

    Starting project, MinimaxAgent and MinimaxABAgent implemented, not well tested

--- agents.py
+++ agents.py
@@ -37,49 +37,84 @@ class GreedyAgent(Agent):
 
 
 def state_evaluation(state, old_state, depth):
-    if depth % 2 == 1:
-        return state.get_score(old_state.get_on_move_chr())
-    else:
-        return -state.get_score(old_state.get_on_move_chr())
+    estimatedScore = state.get_score(old_state.get_on_move_chr()) - state.get_score(state.get_on_move_chr())
+    return estimatedScore if depth % 2 == 1 else -estimatedScore
 
-def id_minimax(state, depth, max_depth, old_state):
-
-    if depth == max_depth:
+def minimax(state, depth, max_depth, old_state):
+    if depth == max_depth or state.is_goal_state():
         return state_evaluation(state, old_state, depth)
 
     best_action = None
-
     if depth % 2 == 0:
         # MAX player
         score = float('-inf')
         best_score = None
         for action in state.get_legal_actions():
             new_state = state.generate_successor_state(action)
-            score = max(score, id_minimax(new_state, depth + 1, max_depth, state))
+            score = max(score, minimax(new_state, depth + 1, max_depth, state))
             if (best_score is None and best_action is None) or score > best_score:
                 best_action = action
                 best_score = score
-        if depth == 0 : return best_action
-        return score
+        return best_action if depth == 0 else score
     else:
         # MIN player
         score = float('inf')
         best_score = None
         for action in state.get_legal_actions():
             new_state = state.generate_successor_state(action)
-            score = min(score, id_minimax(new_state, depth + 1, max_depth, state))
+            score = min(score, minimax(new_state, depth + 1, max_depth, state))
             if (best_score is None and best_action is None) or score < best_score:
                 best_action = action
                 best_score = score
-        if depth == 0 : return best_action
-        return score
+        return best_action if depth == 0 else score
 
 class MinimaxAgent(Agent):
 
     def get_chosen_action(self, state, max_depth):
-        print(type(self))
         time.sleep(0.5)
-        return id_minimax(state, 0, max_depth, state)
+        return minimax(state, 0, max_depth, None)
+
+def minimaxAB(state, depth, max_depth, old_state, a, b):
+    if depth == max_depth or state.is_goal_state():
+        return state_evaluation(state, old_state, depth)
+
+    best_action = None
+    if depth % 2 == 0:
+        # MAX player
+        score = float('-inf')
+        best_score = None
+        for action in state.get_legal_actions():
+            new_state = state.generate_successor_state(action)
+            score = max(score, minimaxAB(new_state, depth + 1, max_depth, state, a, b))
+            if (best_score is None and best_action is None) or score > best_score:
+                best_action = action
+                best_score = score
+            # alpha update
+            a = max(a, score)
+            if a >= b: break
+
+        return best_action if depth == 0 else score
+    else:
+        # MIN player
+        score = float('inf')
+        best_score = None
+        for action in state.get_legal_actions():
+            new_state = state.generate_successor_state(action)
+            score = min(score, minimaxAB(new_state, depth + 1, max_depth, state, a, b))
+            if (best_score is None and best_action is None) or score < best_score:
+                best_action = action
+                best_score = score
+            # beta update
+            b = min(b, score)
+            if a >= b: break
+
+        return best_action if depth == 0 else score
+
+class MinimaxABAgent(Agent):
+
+    def get_chosen_action(self, state, max_depth):
+        time.sleep(0.5)
+        return minimaxAB(state, 0, max_depth, state, float('-inf'), float('inf'))
 
 
 
